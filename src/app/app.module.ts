bc051bf00e41c8285404f6d01856d214db0ce518
test <damiendoucetgirard@gmail.com>
2022-01-15 22:02:36 -0500

    working ng2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CardbookComponent } from './cardbook/cardbook/cardbook.component';
 import { CardbookComponent } from './cardbook/cardbook.component';
 import { DecklistComponent } from './decklist/decklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardbookComponent,
     CardbookComponent,
     DecklistComponent
   ],
--- src/app/cardbook/cardbook.component.ts
+++ src/app/cardbook/cardbook.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
   selector: 'app-cardbook',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cardbook.component.css']
 })
 export class CardbookComponent implements OnInit {
+  title = 'app works!';
+  loading: boolean;
+  data: string;
+  backendUrl = "http://localhost:8000"
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: Http) {
   }
 
+  ngOnInit() {
+    this.cards(1);
+  }
+
+
+  cards(page: number): void {
+    this.loading = true;
+    this.http.request(`${this.backendUrl}/cards?page=${page}`)
+    .subscribe((res: Response) => {
+      this.data = res.json();
+      console.log(this.data)
+      this.loading = false;
+    });
+  }
 }
